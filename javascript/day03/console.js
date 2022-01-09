12e97ab554edbe607de0be3183e3f25b6e50b913
minu <made_power@naver.com>
2022-01-09 14:01:49 +0900

    day05

---
+++ javascript/day03/console.js
@@ -0,0 +1,6 @@
+function print(a, b, c, d) {
+    if (a != undefined && b != undefined && c != undefined && d != undefined)
+        console.log(`${a},${b},${c},${d} = ${a + b + c + d}`);
+
+    return 1;
+}
